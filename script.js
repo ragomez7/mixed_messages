1096cfc09a2714572d041aa296708be146f174e5
Rafael Gómez <satrgomez7@gmail.com>
2021-09-16 18:39:53 -0400

    Added object functionality and created function to generate a random index

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
-const heroes = ["Elon Musk", "Steve Jobs", "Bill Gates", "Peter Norvig", "Andrew Ng"];
-const wishes = ["lots of creativity", "success", "global impact", "constant learning"];
-const carreers = ["AI", "Software", "Machine Learning", "Deep Learning"];
+const generateRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length)
+};
 
-const hero_index = Math.floor(Math.random() * heroes.length)
-const hero_selection = heroes[hero_index]
+const messageComponents = {
+    heroes: ["Elon Musk", "Steve Jobs", "Bill Gates", "Peter Norvig", "Andrew Ng"],
+    wishes: ["lots of creativity", "success", "global impact", "constant learning"],
+    carreers: ["AI", "Software", "Machine Learning", "Deep Learning"]
+}
 
-const wish_index = Math.floor(Math.random() * wishes.length)
-const wish_selection = wishes[wish_index]
-console.log(hero_selection + " wishes you " + wish_selection)
+const hero_index = generateRandomIndex(messageComponents.heroes);
+const hero_selection = messageComponents.heroes[hero_index];
+
+const wish_index = generateRandomIndex(messageComponents.wishes);
+const wish_selection = messageComponents.wishes[wish_index];
+
+const carreer_index = generateRandomIndex(messageComponents.carreers);
+const carreer_selection = messageComponents.carreers[carreer_index];
+
+console.log(hero_selection + " wishes you " + wish_selection + " " + carreer_selection + "!");
 //Elon Musk wishes you lots of creativity in your AI carreer
