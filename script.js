b968a728334c40e60ccddab5e734094320a5625a
Rafael Gómez <satrgomez7@gmail.com>
2021-09-16 18:50:36 -0400

    Added comments to the script

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
+//Function that generates a random index based on the passed array
 const generateRandomIndex = (array) => {
     return Math.floor(Math.random() * array.length)
 };
 
+//Object containing the message components
 const messageComponents = {
     heroes: ["Elon Musk", "Steve Jobs", "Bill Gates", "Peter Norvig", "Andrew Ng"],
     wishes: ["lots of creativity", "success", "global impact", "constant learning"],
     carreers: ["AI", "Software", "Machine Learning", "Deep Learning"]
 }
-
+//Selecting a hero
 const hero_index = generateRandomIndex(messageComponents.heroes);
 const hero_selection = messageComponents.heroes[hero_index];
 
+//Selecting a wish
 const wish_index = generateRandomIndex(messageComponents.wishes);
 const wish_selection = messageComponents.wishes[wish_index];
 
+//Selecting a carreer
 const carreer_index = generateRandomIndex(messageComponents.carreers);
 const carreer_selection = messageComponents.carreers[carreer_index];
 
+//Logging the full message
 console.log(hero_selection + " wishes you " + wish_selection + " " + carreer_selection + "!");
-//Elon Musk wishes you lots of creativity in your AI carreer
